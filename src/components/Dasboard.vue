6fa250e9e604eabf4ccd22ae29726b2b2e561ca2
Jean Everton <sistemas2@leiloesjudiciais.com.br>
2022-04-07 18:48:48 -0300

    estilizando o dasboard de pedidos

---
+++ src/components/Dasboard.vue
@@ -0,0 +1,99 @@
+<template>
+  <div id="burguer-table">
+    <div>
+      <div id="burguer-table-heading">
+        <div class="order-id">#</div>
+        <div>Cliente</div>
+        <div>Pão</div>
+        <div>Carne</div>
+        <div>Opcionais</div>
+        <div>Ações</div>
+      </div>
+      <div id="burger-table-rows">
+        <div class="burger-table-row">
+          <div class="order-number">1</div>
+          <div>Joao</div>
+          <div>Pão de trigo</div>
+          <div>Maminha</div>
+          <div>
+            <ul>
+              <li>Salame</li>
+              <li>bacon</li>
+            </ul>
+          </div>
+          <div>
+            <select name="status" class="status">
+              <option value="">Selecione</option>
+            </select>
+            <button class="delete-btn">Cancelar</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Dasboard"
+}
+</script>
+
+<style scoped>
+#burguer-table {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+
+#burguer-table-heading,
+#burger-table-rows,
+.burger-table-row {
+  display: flex;
+  flex-direction: row;
+}
+
+#burguer-table-heading {
+  font-weight: bold;
+  padding: 12px;
+  border-bottom: 3px solid #333;
+}
+
+#burguer-table-heading div,
+.burger-table-row div {
+  width: 100%;
+}
+
+.burger-table-row {
+  width: 100%;
+  padding: 12px;
+  border-bottom: 1px solid #ccc;
+}
+
+#burguer-table-heading .order-id,
+.burger-table-row .order-number {
+  width: 5%;
+}
+
+select {
+  padding: 12px 6px;
+  margin-right: 12px;
+}
+
+.delete-btn {
+  background: #222;
+  color: #e58d04;
+  font-weight: bold;
+  border: 2px solid #222;
+  padding: 10px;
+  font-size: 16px;
+  margin: 0 auto;
+  cursor: pointer;
+  transition: 0.5s;
+
+}
+
+.delete-btn:hover {
+  background: #e58d04;
+  color: #222222;
+}
+</style>
--- src/views/Pedidos.vue
+++ src/views/Pedidos.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="main-container">
+    <h1>Gerenciar pedidos</h1>
+    <Dasboard/>
   </div>
 </template>
+
+<script>
+
+import Dasboard from "@/components/Dasboard";
+export default {
+  name: "Pedidos",
+  components: {Dasboard}
+
+}
+</script>
