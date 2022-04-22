893ade0d79561cb6004d73943370db9cb33f2e40
Jean Everton <sistemas2@leiloesjudiciais.com.br>
2022-04-22 10:47:57 -0300

    Atividade trazendo da api as opções do select dinâmicamente.

--- src/components/Dasboard.vue
+++ src/components/Dasboard.vue
@@ -23,15 +23,15 @@
             </ul>
           </div>
           <div>
-            <select name="status" class="status">
+            <select name="status" class="status" @change="updatedBurger($event, burger.id)">
               <option v-for="s in status"
                       :key="s.id"
-                      value="s.tipo"
+                      :value="s.tipo"
                       :selected="burger.status == s.tipo">
               {{ s.tipo }}
               </option>
             </select>
-            <button class="delete-btn">Cancelar</button>
+            <button class="delete-btn" @click="deleteBurger(burger.id)">Cancelar</button>
           </div>
         </div>
       </div>
@@ -70,6 +70,35 @@ export default {
 
       this.status = data;
 
+    },
+    async deleteBurger(id)  {
+
+      const  req = await fetch(`http://localhost:3000/burgers/${id}`, {
+        method: "DELETE"
+      });
+
+      const res = await req.json();
+
+      // msg
+
+      this.getPedidos();
+
+    },
+    async updatedBurger( event, id ){
+
+      const option = event.target.value;
+
+      const dataJson = JSON.stringify({ status: option });
+
+      const req = await fetch(`http://localhost:3000/bugers/${id}`, {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json"},
+        body: dataJson
+      });
+
+      const res = await req.json();
+
+      console.log(res);
     }
   },
   mounted() {
