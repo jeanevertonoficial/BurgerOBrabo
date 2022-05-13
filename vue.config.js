8ba09279ec74fe81541a3c949ed61b4cb9379ba8
Jean Everton <sistemas2@leiloesjudiciais.com.br>
2022-05-13 08:36:29 -0300

    Deploy do projeto.

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/production-sub-path/'
+      : '/',
   transpileDependencies: true
 })
